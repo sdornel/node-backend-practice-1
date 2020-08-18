4db425b3856bfc9c8ff915ae917919df1f535cc6
kaidornel <kai_dornel@hotmail.com>
2020-08-18 19:10:40 -0400

    building node backend

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-// console.log that server is up and running
+// console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
-    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'})
-})
+    res.send(
+        [
+            { 
+                express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' 
+            },
+            {
+                lordOfTheRings: {
+                    author: "Tolkien",
+                    pages: 1000
+                }
+            }
+        ]
+    );
+});
