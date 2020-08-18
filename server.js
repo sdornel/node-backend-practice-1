1fb7e3f72d734dd08867e4d52c95f715c339508c
kaidornel <kai_dornel@hotmail.com>
2020-08-18 19:56:04 -0400

    set cors to allow all

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 3000;
+
+app.use(cors()) // use cors??
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
